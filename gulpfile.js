c20dd9b323e7b56c744ba312424cfb72bb25fcb3
Marek Gańko <ganko.marek@gmail.com>
2015-06-20 21:10:56 +0200

    add gulpPlugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,6 @@
 var gulp = require('gulp');
-var gutil = require('gulp-util');
-var bower = require('bower');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var sh = require('shelljs');
 var gulpPlugins = require('gulp-load-plugins')();
+var gutil = gulpPlugins.util;
 
 var paths = {
   sass: ['./scss/**/*.scss'],
@@ -20,14 +14,14 @@ gulp.task('default', ['sass']);
 
 gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
-    .pipe(sass({
+    .pipe(gulpPlugins.sass({
       errLogToConsole: true
     }))
     .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
+    .pipe(gulpPlugins.minifyCss({
       keepSpecialComments: 0
     }))
-    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulpPlugins.rename({ extname: '.min.css' }))
     .pipe(gulp.dest('./www/css/'))
     .on('end', done);
 });
@@ -37,14 +31,14 @@ gulp.task('watch', function() {
 });
 
 gulp.task('install', ['git-check'], function() {
-  return bower.commands.install()
+  return gulpPlugins.bower.commands.install()
     .on('log', function(data) {
       gutil.log('bower', gutil.colors.cyan(data.id), data.message);
     });
 });
 
 gulp.task('git-check', function(done) {
-  if (!sh.which('git')) {
+  if (!gulpPlugins.shelljs.which('git')) {
     console.log(
       '  ' + gutil.colors.red('Git is not installed.'),
       '\n  Git, the version control system, is required to download Ionic.',
