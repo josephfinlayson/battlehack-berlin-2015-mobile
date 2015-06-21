515d655f432d01f751566b21ac3ef01cf20176f2
Tomusdrw <tomusdrw@gmail.com>
2015-06-21 02:59:02 +0200

    Displaying random charity at the bottom.

--- src/js/components/map/mapComponent.js
+++ src/js/components/map/mapComponent.js
@@ -4,24 +4,47 @@ import template from './mapView.html!text';
 import './mapStyles.css!';
 import _ from 'lodash';
 
-componentsModule.directive('pifMapComponent', (Charities) => {
-	return {
-		template,
-		link: (scope) => {
-			scope.$on('mapInitialized', (event, map) => {
-					map.setCenter(new google.maps.LatLng(52, 13));
-					map.setZoom(10);
-					const charities = Charities.getCharities();
-					_.each(charities, (element) => {
-						new google.maps.Marker({
-							position: new google.maps.LatLng(element.lat, element.lng),
-							map: map,
-							title: 'Hello World!'
-						});
-					});
-				}
-			);
+componentsModule.directive('pifMapComponent', (Charities, $timeout) => {
+  return {
+    template,
+    controllerAs: 'vm',
+    link: (scope) => {
 
-		}
-	};
+      let isDestroyed;
+
+      function rollCharity() {
+        let a = scope.charities;
+        scope.currentCharity = a[Math.floor(Math.random() * a.length)];
+        if (isDestroyed) {
+          return;
+        }
+
+        $timeout(rollCharity, 5000);
+      }
+
+      scope.$on('$destroy', ()=>{
+        isDestroyed = true;
+      });
+
+      scope.$on('mapInitialized', (event, map) => {
+        map.setCenter(new google.maps.LatLng(52, 13));
+        map.setZoom(10);
+        Charities.getCharities().then((charities)=>{
+
+          scope.charities = charities;
+
+          rollCharity();
+
+          _.each(charities, (element) => {
+            new google.maps.Marker({
+                position: new google.maps.LatLng(element.lat, element.lng),
+                map: map,
+                title: 'Hello World!'
+            });
+          });
+        });
+      });
+
+    }
+  };
 });
